1b4eae345021583612999bc472aaf495786628bd
Carsten Nielsen <heycarsten@gmail.com>
2017-05-04 14:41:58 -0400

    Explain the insane

--- src/ui/components/aptry/endpoints-dropdown/component.ts
+++ src/ui/components/aptry/endpoints-dropdown/component.ts
@@ -15,6 +15,9 @@ export default class EndpointsDropdown extends Component {
     return true;
   }
 
+  // Ability to do addEventListener('click', () => {}, { once: true }) would be
+  // better, a background overlay div that listens for a click would probably
+  // be good too
   didInsertElement() {
     document.addEventListener('click', this, false);
   }
